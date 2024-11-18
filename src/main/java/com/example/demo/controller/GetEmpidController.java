66ee7573824c2347d580892cb232e5a17cc7f8b7
Ho-Jin-Jeong <hj2jeong@gmail.com>
2024-11-19 02:17:52 +0900

    사번조회

--- src/main/java/com/example/demo/controller/GetEmpidController.java
+++ src/main/java/com/example/demo/controller/GetEmpidController.java
@@ -1,4 +1,25 @@
 package com.example.demo.controller;
 
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequestMapping("/api/v1")
+@RestController
 public class GetEmpidController {
+    @GetMapping("/user")
+    public ResponseEntity<String> getUserInfo() {
+        return ResponseEntity.ok("82022571");
+    }
 }
+
+
+
+
+
+
